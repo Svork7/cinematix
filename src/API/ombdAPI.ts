13420fa97813fe5cfbbe9a44ff8bb175ba30cbe4
Svork7 <svork@ya.ru>
2023-03-07 18:38:53 +0300

    api redone

--- src/API/ombdAPI.ts
+++ src/API/ombdAPI.ts
@@ -1,8 +1,10 @@
 /* для себя, чтобы понимать какие данные отдает апишка
 {"Title":"Sunshine","Year":"2007","Rated":"R","Released":"27 Jul 2007","Runtime":"107 min","Genre":"Sci-Fi, Thriller","Director":"Danny Boyle","Writer":"Alex Garland","Actors":"Cillian Murphy, Rose Byrne, Chris Evans","Plot":"A team of international astronauts is sent on a dangerous mission to reignite the dying Sun with a nuclear fission bomb in 2057.","Language":"English","Country":"United Kingdom, United States","Awards":"1 win & 23 nominations","Poster":"https://m.media-amazon.com/images/M/MV5BMDliNGY4ZGQtMjU5MS00ODhjLWExMDEtNzI1NmIwZDkzZWQwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.2/10"},{"Source":"Rotten Tomatoes","Value":"76%"},{"Source":"Metacritic","Value":"64/100"}],"Metascore":"64","imdbRating":"7.2","imdbVotes":"254,462","imdbID":"tt0448134","Type":"movie","DVD":"08 Jan 2008","BoxOffice":"$3,675,753","Production":"Fox","Website":"N/A","Response":"True"}
 */
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { API_KEY, OMDB_API_URL } from '../app/constants'
+import { SearchType } from '../app/types'
 
 // Тип для объекта фильма
 export interface MovieInfo {
@@ -24,7 +26,6 @@ export interface MovieInfo {
 
 // Тип для объекта ответа от API
 export interface OmdbAPIResponse {
-  //тип отдаваемых данных
   Search: MovieInfo[]
   totalResults: string
   Response: string
@@ -43,14 +44,13 @@ export const omdbAPI = createApi({
       query: () => ({
         url: `?apikey=${API_KEY}&s=sunshine`,
       }),
-      transformResponse: (data: OmdbAPIResponse) => {
-        // преобразовываем ответ API в список фильмов
-        return data.search.map((movie) => {
-          return Object.keys(movie).reduce((acc, key) => {
+      transformResponse: (data: OmdbAPIResponse): MovieInfo[] => {
+        return data.Search.map((movie) =>
+          Object.keys(movie).reduce((acc, key) => {
             acc[key.toLowerCase()] = movie[key]
             return acc
           }, {} as MovieInfo)
-        })
+        )
       },
     }),
 
@@ -59,33 +59,29 @@ export const omdbAPI = createApi({
       query: (query) => ({
         url: `?apikey=${API_KEY}&t=${query}&plot=full`,
       }),
-      transformResponse: (data: MovieInfo) => {
-        // преобразовываем ответ API в объект с информацией о фильме
-        return Object.keys(data).reduce((acc, key) => {
+      transformResponse: (data: MovieInfo): MovieInfo =>
+        Object.keys(data).reduce((acc, key) => {
           acc[key.toLowerCase()] = data[key]
           return acc
-        }, {} as MovieInfo)
-      },
+        }, {} as MovieInfo),
     }),
 
-    // Поиск фильмов по запросу
+    // поиск фильмов по запросу
     fetchMovies: build.query<MovieInfo[], string>({
       query: (query) => ({
         url: `?apikey=${API_KEY}&s=${query}`,
       }),
-      transformResponse: (data: OmdbAPIResponse) => {
-        // Если поиск не дал результатов, вернуть пустой массив
-        // ответ API {"Response":"False","Error":"Movie not found!"}
+      transformResponse: (data: OmdbAPIResponse): MovieInfo[] => {
         if (data.Response === 'False' && data.Error === 'Movie not found!') {
           return []
         } else {
           // ответ API в список фильмов
-          return data.search.map((movie) => {
-            return Object.keys(movie).reduce((acc, key) => {
+          return data.Search.map((movie) =>
+            Object.keys(movie).reduce((acc, key) => {
               acc[key.toLowerCase()] = movie[key]
               return acc
             }, {} as MovieInfo)
-          })
+          )
         }
       },
     }),
