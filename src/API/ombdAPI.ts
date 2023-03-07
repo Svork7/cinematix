a68877717d3bf4e4a6282bcc264ea68870d01dd0
Svork7 <svork@ya.ru>
2023-03-07 15:55:43 +0300

    api redone

--- src/API/ombdAPI.ts
+++ src/API/ombdAPI.ts
@@ -1,18 +1,10 @@
 /* для себя, чтобы понимать какие данные отдает апишка
 {"Title":"Sunshine","Year":"2007","Rated":"R","Released":"27 Jul 2007","Runtime":"107 min","Genre":"Sci-Fi, Thriller","Director":"Danny Boyle","Writer":"Alex Garland","Actors":"Cillian Murphy, Rose Byrne, Chris Evans","Plot":"A team of international astronauts is sent on a dangerous mission to reignite the dying Sun with a nuclear fission bomb in 2057.","Language":"English","Country":"United Kingdom, United States","Awards":"1 win & 23 nominations","Poster":"https://m.media-amazon.com/images/M/MV5BMDliNGY4ZGQtMjU5MS00ODhjLWExMDEtNzI1NmIwZDkzZWQwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.2/10"},{"Source":"Rotten Tomatoes","Value":"76%"},{"Source":"Metacritic","Value":"64/100"}],"Metascore":"64","imdbRating":"7.2","imdbVotes":"254,462","imdbID":"tt0448134","Type":"movie","DVD":"08 Jan 2008","BoxOffice":"$3,675,753","Production":"Fox","Website":"N/A","Response":"True"}
 */
-
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { SearchTypes } from '../app/types'
-import { API_KEY } from '../app/constants'
+import { API_KEY, OMDB_API_URL } from '../app/constants'
 
-export interface SearchResponse {
-  SearchTypes: Record<string, SearchTypes>[]
-  totalResults: string
-  Response: string
-  [propName: string]: any
-}
-
+// Тип для объекта фильма
 export interface MovieInfo {
   Title: string
   Year: string
@@ -30,69 +22,66 @@ export interface MovieInfo {
   Website: string
 }
 
-export const ombdAPI = createApi({
+// Тип для объекта ответа от API
+export interface OmdbApiResponse {
+  //тип отдаваемых данных
+  Search: MovieInfo[]
+  totalResults: string
+  Response: string
+  [propName: string]: any
+}
+
+// Создаем экземпляр API с помощью createApi
+export const omdbApi = createApi({
   reducerPath: 'movies',
-  baseQuery: fetchBaseQuery({ baseUrl: `http://www.omdbapi.com/` }),
+  baseQuery: fetchBaseQuery({ baseUrl: OMDB_API_URL }),
+
+  // endpoint-ы для запросов к API
   endpoints: (build) => ({
-    fetchAllMovies: build.query<Record<string, any>[], void>({
+    // Получаем список всех фильмов
+    fetchAllMovies: build.query<MovieInfo[], void>({
       query: () => ({
         url: `?apikey=${API_KEY}&s=sunshine`,
       }),
-      transformResponse: (data: SearchResponse) => {
-        const handledResponse = []
-
-        for (let i = 0; i < data.SearchTypes.length; i++) {
-          let obj: Record<string, SearchTypes> = {}
-          for (let key in data.SearchTypes[i]) {
-            obj[key[0].toLowerCase() + key.slice(1)] = data.SearchTypes[i][key]
-          }
-          handledResponse.push(obj)
-        }
-
-        return handledResponse
+      transformResponse: (data: OmdbApiResponse) => {
+        // преобразовываем ответ API в список фильмов
+        return data.Search
       },
     }),
 
-    fetchMovie: build.query<any, string>({
+    // Получаем информацию о конкретном фильме по его названию
+    fetchMovie: build.query<MovieInfo, string>({
       query: (query) => ({
         url: `?apikey=${API_KEY}&t=${query}&plot=full`,
       }),
-      transformResponse: (data: Array<MovieInfo>) => {
-        const handledResponse: Record<string, MovieInfo> = {}
-
-        for (let key in data) {
-          handledResponse[key.toLowerCase()] = data[key]
-        }
-
-        return handledResponse
+      transformResponse: (data: MovieInfo) => {
+        // преобразовываем ответ API в объект с информацией о фильме
+        return data
       },
     }),
-    fetchMovies: build.query({
+
+    // Поиск фильмов по запросу
+    fetchMovies: build.query<MovieInfo[], string>({
       query: (query) => ({
         url: `?apikey=${API_KEY}&s=${query}`,
       }),
-      transformResponse: (data: SearchResponse) => {
-        const handledResponse: Array<Record<string, SearchTypes>> = []
-        if (data.Error === 'Sorry, we did not find anything') {
-          return handledResponse
+      transformResponse: (data: OmdbApiResponse) => {
+        // Если поиск не дал результатов, вернуть пустой массив
+        // ответ API {"Response":"False","Error":"Movie not found!"}
+        if (data.Error === 'Movie not found!') {
+          return []
         } else {
-          for (let i = 0; i < data.SearchTypes.length; i++) {
-            let obj: Record<string, SearchTypes> = {}
-            for (let key in data.SearchTypes[i]) {
-              obj[key[0].toLowerCase() + key.slice(1)] =
-                data.SearchTypes[i][key]
-            }
-            handledResponse.push(obj)
-          }
-          return handledResponse
+          // ответ API в список фильмов
+          return data.Search
         }
       },
     }),
   }),
 })
 
+// Экспортируем утилиты для использования в приложении
 export const {
   useFetchAllMoviesQuery,
   useFetchMovieQuery,
   useFetchMoviesQuery,
-} = ombdAPI
+} = omdbApi
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,4 @@
+// ключ API
 export const API_KEY = '1a9307d6'
+// url API
+export const OMDB_API_URL = 'http://www.omdbapi.com/'
